17ce12b2d7c940abf0f3915af59de64aac2d47fe
Carlos <cgome032@ucr.edu>
2017-11-26 22:14:50 +0000

    Changing formatting of system

--- src/navigation.py
+++ src/navigation.py
@@ -47,19 +47,54 @@ disp.begin()
 # Background will be set to green
 disp.clear((0,255,0))
 
-rVal = 0
-gVal = 0
-bVal = 0
 
-while True:
-    disp.clear((rVal,gVal,bVal))
-    disp.display()
-    time.sleep(.5) # Wait time will be taken in seconds
-    rVal += 10
-    gVal += 10
-    bVal += 10
-    if(rVal > 255):
-        rVal = 0
-        gVal = 0
-        bVal = 0
+# Clear screen to while and display
+disp.clear((255,255,255))
+draw = disp.draw()
+draw.rectangle((0,10,127,150),outline=(255,0,0),fill=(0,0,255))
+disp.display()
+
+# Speed, Latitude, Longitude placement variables
+currentS = "CURRENT SPEED: "
+speedX = 10
+speedY = 20
+
+# Speed update function, returns string
+def speedFunc():
+    SpeedText = data_stream.TPV['speed']
+    return str(SpeedText)
+
+
+# Latitude update function, returns string
+def latFunc():
+    Latitude = data_stream.TPV['lat']
+    return str(Latitude)
+
+# Longitude update function, returns string 
+def lonFunc():
+    Longitude = data_stream.TPV['lon']
+    return str(Longitude)
+
+# Screen output function
+def output():
+    disp.clear((255,255,255))
+    draw.rectangle((0,10,127,150),outline=(255,0,0),fill=(255,0,0))
+
+
+# Setup gps
+gps_socket=gps3.GPSDSocket()
+data_stream=gps3.DataStream()
+gps_socket.connect()
+gps_socket.watch()
+for new_data in gps_socket:
+    if new_data:
+        data_stream.unpack(new_data)
+        #SpeedText = data_stream.TPV['speed']
+        #Latitude = data_stream.TPV['lat']
+        #Longitude = data_stream.TPV['lon']
+        draw.text((speedX,speedY),str(SpeedText),font=ImageFont.load_default())
+        print('Latitude = ',data_stream.TPV['lat'])
+        print('Longitude = ',data_stream.TPV['lon'])
+        disp.display()
+        time.sleep(1) # Wait time will be taken in seconds
 
