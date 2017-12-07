035981f83e91710b4676ea61fac3cc1bb7c5ac73
Carlos <cgome032@ucr.edu>
2017-12-07 04:26:10 +0000

    Testing navigation

---
+++ src/exNav.py
@@ -0,0 +1,28 @@
+import time
+
+from gps3 import gps3
+
+gpsd_socket = gps3.GPSDSocket()
+gpsd_socket.connect()
+gpsd_socket.watch()
+data_stream = gps3.DataStream()
+
+try:
+    for new_data in gpsd_socket:
+        if new_data:
+            data_stream.unpack(new_data)
+        if data_stream.TPV['lat'] != 'n/a':
+            speed = data_stream.TPV['speed']
+            latitude = data_stream.TPV['lat']
+            longitude = data_stream.TPV['lon']
+            altitude = data_stream.TPV['alt']
+
+        else:
+            print('Nothin in gps')
+            time.sleep(.1)
+        time.sleep(.8)
+
+except KeyboardInterrupt:
+    gpsd_socket.close()
+    print('\nTerminated by user\nGood Bye.\n')
+
